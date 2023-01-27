af86f689c21d8956740c8635fe7c6e7af0ac7a4a
Kalki <kalkithewarrior2706@proton.me>
2023-01-28 03:56:36 +0530

    Maths & Variables
    arithmetic operations & variables

---
+++ arithmetic&varicables.py
@@ -0,0 +1,76 @@
+# Printing my First Program
+print("Gonna Become A Beast!")
+
+# Arithmetic Operations
+
+# Addition
+print(1 + 2)
+
+# Subtraction
+print(9 - 5)
+
+# Multiplication
+print(2 * 4)
+
+# Dividation
+print(6 / 3)
+
+# Exponent
+print(3 ** 2)
+
+# PEDMAS RULE
+print(((1 + 3) * (9 - 2) / 2) ** 2)
+# = (4 * 7 / 2) ** 2
+# = (4 * 3.5) ** 2
+# = 14 ** 2
+# = 196.0
+
+# Using comments
+# Multiply 3 * 2
+print(3 * 2)
+
+# VARIABLES
+
+# Creating Variables
+a = 5
+b = 4
+
+test_var = a + b
+print(test_var)
+
+# Manipulating variables
+
+# Value set 3
+my_var = 3
+
+# print the value
+print(my_var)
+
+# Change the value to 100
+my_var = 100
+
+# Print new value
+print(my_var)
+
+# Increase the value by 3 of my_var
+my_var = my_var + 3
+
+# Print the value
+print(my_var)
+
+# Multiple Variables
+
+# Create variables
+num_years = 4
+days_per_year = 365
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+
+# Update to include leap years
+days_per_year = 365.25
+
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+
+print(total_secs)
