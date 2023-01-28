3ef261122dd6e7b0f56cf99bbd027366ed6b3e47
Kalki <kalkithewarrior2706@proton.me>
2023-01-28 17:48:00 +0530

    declared variable
    will take input var and print output var

--- IntroToProgramming/functions.py
+++ IntroToProgramming/functions.py
@@ -2,9 +2,15 @@
 
 # Def is a function and add_three is a function name and inside parentheses arugument is passed for taking input and ends with colon : The def line comes under HEAD SECTION
 def add_three(input_var):
-    
+
     # output var is gonna give output by adding 3 in input variable AFTER THE COLON EVERYTHING UNDER DEF IS BODY SECTION
     output_var = input_var + 3
 
     # return is a keyword which is gonna return the value of output var
     return output_var
+
+# declared new_num variable to ouput & the input var added value in add_three def 
+new_num = add_three(10)
+
+# Printing value by adding input_var + 3 which will be 13
+print(new_num)
