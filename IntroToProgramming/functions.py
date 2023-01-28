a01888d2cce0d7fcde9b79735d726e26bb3f0111
Kalki <kalkithewarrior2706@proton.me>
2023-01-28 17:35:20 +0530

    What's a Function

---
+++ IntroToProgramming/functions.py
@@ -0,0 +1,10 @@
+# Defining a function: def - define 
+
+# Def is a function and add_three is a function name and inside parentheses arugument is passed for taking input and ends with colon : The def line comes under HEAD SECTION
+def add_three(input_var):
+    
+    # output var is gonna give output by adding 3 in input variable AFTER THE COLON EVERYTHING UNDER DEF IS BODY SECTION
+    output_var = input_var + 3
+
+    # return is a keyword which is gonna return the value of output var
+    return output_var
