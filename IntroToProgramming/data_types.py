50d2b1f666e2306ebef827cd38aede001a8a7f65
Kalki <kalkithewarrior2706@proton.me>
2023-01-29 22:15:26 +0530

    int data type

---
+++ IntroToProgramming/data_types.py
@@ -0,0 +1,9 @@
+# Whenever you create a variable in Python, it has a value with a corresponding data type.
+
+# Integers
+# numbers without any fractional part and can be positive or negative
+# We can use type() function to know which type of data type is being used in code.
+
+x = 14
+print(x)
+print(type(x))
