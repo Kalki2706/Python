0da1319a01240b7a86701d4a4c5ce7f8aab15a5f
Kalki <72976312+Kalki2706@users.noreply.github.com>
2023-01-30 00:45:05 +0530

    float variable

--- IntroToProgramming/data_types.py
+++ IntroToProgramming/data_types.py
@@ -5,5 +5,22 @@
 # We can use type() function to know which type of data type is being used in code.
 
 x = 14
+
 print(x)
 print(type(x))
+
+
+# Floats 
+# Floats are numbers with fractional parts. They can have many numbers after decimal.
+
+nearly_pi = 3.141592653589793238462643383279502884197169399375105820974944
+
+print(nearly_pi)
+print(type(nearly_pi))
+
+# We can also specify a float with a fraction. 
+
+almost_pi = 22 / 7
+
+print(almost_pi)
+print(type(almost_pi))
