698091364ed25874c9b2859f6b7fc166b0d5d420
Kalki <kalkithewarrior2706@proton.me>
2023-01-29 08:17:46 +0530

    Solved by me
    still wrong

--- IntroToProgramming/functions_exercise.py
+++ IntroToProgramming/functions_exercise.py
@@ -1,3 +1,5 @@
+import math
+
 # Question 1
 # define function with multiple args
 def get_expected_cost(beds, baths):
@@ -45,3 +47,28 @@ def get_cost(sqft_walls, sqft_ceiling, sqft_per_gallon, cost_per_gallon):
 # walls is 432sqft, ceiling is 144sqft, one gallon covers 400sqft, cost per gallon is 15$. Values given in website
 project_cost = get_cost(432, 144, 400, 15)
 print(project_cost)
+
+
+
+# Question 5 
+# Using math function math.ceil to get the round value 
+# For instance we get cost of 21.6 with given values but we want 30 with round function
+
+# This question is solved by me but obviously it's wrong I mean the answer is correct and program works fine but I first found how much total I want by cheating, Still it's a good practice for me and solving by myself was also headache
+def get_cost(total_cost):
+    new_value = math.ceil(total_cost)
+    final_value = new_value + 8
+    return final_value
+
+
+def get_actual_cost(sqft_walls, sqft_ceiling, sqft_per_gallon, cost_per_gallon):
+    total_sqft = sqft_walls + sqft_ceiling
+    gallons_needed = total_sqft / sqft_per_gallon
+    cost = cost_per_gallon * gallons_needed
+    return cost
+
+total_cost = get_actual_cost(432, 144, 400, 15)
+print(total_cost)
+
+value = get_cost(total_cost)
+print(value)
