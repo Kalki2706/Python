d5ac169ca1d01d990bfb56c34fff50baa5ce8229
Kalki <kalkithewarrior2706@proton.me>
2023-01-30 17:19:59 +0530

    Question 3

--- IntroToProgramming/data_types_exercise.py
+++ IntroToProgramming/data_types_exercise.py
@@ -13,3 +13,17 @@ z = int(y)
 print(z)
 print(type(z))
 
+# Question 2
+
+print(3 * True)
+
+print(-3.1 * True)
+
+print(type("abc" * False)) 
+
+print(len("abc" * False))
+
+name = "abc " * False
+print(name)
+
+# When you multiple an integer or float by a boolean with value True, it just returns that same integer or float (and is equivalent to multiplying by 1). If you multiply an integer or float by a boolean with value False, it always returns 0. This is true for both positive and negative numbers. If you multiply a string by a boolean with value True, it just returns that same string. And if you multiply a string by a boolean with value False, it returns an empty string (or a string with length zero).
