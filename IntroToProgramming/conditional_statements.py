a2009169d8b6b5bcf925657fadcb515398004ee7
Kalki <kalkithewarrior2706@proton.me>
2023-01-30 21:25:47 +0530

    conditions

---
+++ IntroToProgramming/conditional_statements.py
@@ -0,0 +1,8 @@
+# Conditions & Conditional Statements
+
+# Conditions are based on if else statement & It's a boolean type. If the condition is true it will print the true value else the condition will print false
+
+a = 2 > 3
+
+print(a)
+print(type(a))
