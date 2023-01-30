f28881930a410bb2fd34cf59e33d7839df90150d
Kalki <72976312+Kalki2706@users.noreply.github.com>
2023-01-30 14:26:19 +0530

    round function in float

---
+++ data_types.py
@@ -0,0 +1,26 @@
+# Whenever you create a variable in Python, it has a value with a corresponding data type.
+
+# Integers
+# numbers without any fractional part and can be positive or negative
+# We can use type() function to know which type of data type is being used in code.
+
+x = 14
+
+print(x)
+print(type(x))
+
+
+# Floats 
+# Floats are numbers with fractional parts. They can have many numbers after decimal.
+
+nearly_pi = 3.141592653589793238462643383279502884197169399375105820974944
+
+print(nearly_pi)
+print(type(nearly_pi))
+
+# We can also specify a float with a fraction. 
+
+almost_pi = 22 / 7
+
+print(almost_pi)
+print(type(almost_pi))
