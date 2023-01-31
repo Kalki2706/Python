202d224beb79fa58910ef2b0adee2fbc02ded88c
Kalki <kalkithewarrior2706@proton.me>
2023-01-31 21:54:33 +0530

    practicing variables and arithmetic

--- IntroToProgramming/practice.py
+++ IntroToProgramming/practice.py
@@ -3,3 +3,18 @@
 a = 1 
 b = 2 
 print(a + b)
+
+
+var_a = 10
+var_b = 10
+
+print(var_a ** var_b)
+
+var_y = 30
+var_z = 10
+
+print(var_y / var_z)
+
+
+# Variable means name that we give to store data 
+# var_a & var_b is a variable to store data.
