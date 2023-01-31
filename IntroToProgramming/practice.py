700bbacb674d3351b2e6360dfb22a24c2523d865
Kalki <kalkithewarrior2706@proton.me>
2023-01-31 22:11:57 +0530

    data types

--- IntroToProgramming/practice.py
+++ IntroToProgramming/practice.py
@@ -18,3 +18,18 @@ print(var_y / var_z)
 
 # Variable means name that we give to store data 
 # var_a & var_b is a variable to store data.
+
+
+# Data types
+
+# Int - stores only number without fraction for eg. 1, 0, -1 
+
+# float - stores only fraction number 1.0, -1.0
+
+# boolean - True or false
+
+# String - "kalki" stores text
+
+# you can only add string with string
+
+# You can only multiply string with int others will throw an error.
