f91e34f2c3da9c10e16aaeee188ff8bd37162301
Kalki <kalkithewarrior2706@proton.me>
2023-01-31 13:56:34 +0530

    if statement

--- IntroToProgramming/conditional_statements.py
+++ IntroToProgramming/conditional_statements.py
@@ -25,3 +25,31 @@ var_two = 2
 
 print(var_one < 1)
 print(type(var_one))
+
+# Commom symbols you can use to construct conditions
+# == equals, != does not equal, < less than
+# <= less than equal, > greater than, >= greater than equal
+
+var_one == 1
+print(var_one)
+
+# Conditional statements use conditions to modify how your function runs. They check the value of a condition, and if the condition evaluates to True, then a certain block of code is executed. (Otherwise, if the condition is False, then the code is not run.
+
+
+# "if" statement 
+
+def evaluate_temp(temp):
+    # Set an initial message
+    message = "Normal temprature."
+
+    # Update value of message only if temprature greater than 38
+    if temp > 38:
+        message = "Fever!"
+
+    return message
+
+fever = evaluate_temp(40)
+print(fever)
+
+# We can call function without declaring variable
+# print(evaluate_temp(38))
