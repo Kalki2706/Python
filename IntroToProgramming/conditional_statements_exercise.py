71dd761c3e1b0f69254948537f04684d93beab5f
Kalki <72976312+Kalki2706@users.noreply.github.com>
2023-02-04 19:03:33 +0530

    Update conditional_statements_exercise.py

--- IntroToProgramming/conditional_statements_exercise.py
+++ IntroToProgramming/conditional_statements_exercise.py
@@ -4,3 +4,16 @@
 def get_grade(score):
   if score >= 90:
     grade = "A"
+  elif score >= 80:
+    grade = "B"
+  elif score >= 70:
+    grade = "C"
+  elif score >= 60:
+    grade = "D"
+  else:
+    grade = "F"
+    
+  return grade 
+
+
+
